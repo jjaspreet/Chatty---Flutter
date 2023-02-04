1cc7a25654bd4a9df7b8e21c35870434a0d0f2c5
Jaspreet Singh <jaspresingh@suncor.com>
2023-02-04 15:12:49 +0530

    Initial commit - Added the respective packages.

---
+++ lib/pages/frame/welcome/controller.dart
@@ -0,0 +1,10 @@
+import 'package:chat_app/pages/frame/welcome/state.dart';
+import 'package:get/get.dart';
+
+class WelcomeController extends GetxController{
+
+  WelcomeController();
+
+  final title = "Chatty .";
+  final welcomeState = WelcomeState();
+}
